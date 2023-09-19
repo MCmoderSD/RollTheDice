422fd1dfbf2044d3242cbfc6b98263df134b2c03
mcmodersd <mcmodersd@web.de>
2023-09-19 13:36:14 +0200

    better Ui

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -5,11 +5,10 @@ public class GUI extends JFrame {
     private DicePanel dicePanel;
     private JLabel diceLabel;
     public GUI(Controller controller, Dice dice) {
-        initFrame(dice);
-        initComponents(controller);
+        initComponents(controller, dice);
     }
 
-    private void initComponents(Controller controller) {
+    private void initComponents(Controller controller, Dice dice) {
         Font font = new Font("Roboto", Font.BOLD, 18);
 
         JButton rollButton = new JButton("Roll The Dice");
@@ -20,51 +19,37 @@ public class GUI extends JFrame {
         rollButton.setBounds((getWidth()-width) / 2, (getHeight()-height) /4, width, height);
         rollButton.setFont(font);
         rollButton.addActionListener(e -> controller.buttonPressed(dicePanel));
-        add(rollButton);
+
+        JPanel contentPanel = new JPanel();
+        contentPanel.setPreferredSize(new Dimension(300,200));
+        contentPanel.add(rollButton);
+        contentPanel.setLayout(null);
 
         diceLabel = new JLabel("Dice: ");
         diceLabel.setBounds((getWidth()-width) / 2, ((getHeight()-height) /4)*2, width, height);
         diceLabel.setFont(font);
-        add(diceLabel);
-    }
+        contentPanel.add(diceLabel);
 
-    private void initFrame(Dice dice) {
-        setTitle("Roll The Dice");
-        setSize(300, 200);
-        setLocationRelativeTo(null);
-        setLayout(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setResizable(false);
+
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-
-
-        JFrame diceFrame = new JFrame("Dice");
-        setLayout(null);
-        setLocationRelativeTo(null);
-        setResizable(false);
-
         dicePanel = new DicePanel(dice);
         dicePanel.setPreferredSize(new Dimension(250, 250));
-        diceFrame.add(dicePanel);
-        diceFrame.pack();
 
-
-        int x = ((screenSize.width - this.getWidth()) / 2);
-        int y = ((screenSize.height - this.getHeight() - diceFrame.getHeight()) / 2);
+        int x = ((screenSize.width - (200+250)) / 2);
+        int y = ((screenSize.height - (300+250)) / 2);
 
         setLocation(x, y);
+        Container cp = this.getContentPane();
+
+        cp.add(contentPanel, BorderLayout.NORTH);
+        cp.add(dicePanel, BorderLayout.CENTER);
+
+        pack();
+        Toolkit.getDefaultToolkit().sync();
         setVisible(true);
-
-
-        x = ((screenSize.width - diceFrame.getWidth()) / 2);
-        y = y + this.getHeight();
-
-        diceFrame.setLocation(x, y);
-        diceFrame.setVisible(true);
     }
-
     public void setDiceLabel(String text) {
         diceLabel.setText(text);
     }
